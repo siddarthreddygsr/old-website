0af784beed7ae7037bb96eebe0247efd810360ad
Siddarth Reddy <siddarthreddy4923@gmail.com>
2021-07-03 23:33:30 +0530

    added base files

---
+++ index.js
@@ -0,0 +1,34 @@
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
+$(window).on("load",function(){
+  $(".loader-container").fadeOut(1000);
+});
+
+const tabs = document.querySelector('.tabs');
+
+tabs.addEventListener('click', e => handleClick(e));
+
+function handleClick(e) {
+  const target = e.target;
+  const tabNum = target.dataset.tab;
+  const activeTab = document.querySelector('.tabs .active');
+  const activeContent = document.querySelector(
+    '.content .visible'
+  );
+  const currentContent = document.querySelector(
+    `.content__section[data-tab='${tabNum}']`
+  );
+  
+  if (!tabNum) {
+    return;
+  }
+  
+  activeTab.classList.remove('active');
+  target.classList.add('active');
+  activeContent.classList.remove('visible');
+  currentContent.classList.add('visible');
+}
